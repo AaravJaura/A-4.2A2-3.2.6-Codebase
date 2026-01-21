e0472781782a63e832fc68ed903d05034b2f0cb8
Aarav Jaura <aaravjaura@gmail.com>
2026-01-21 14:23:44 -0800

    finished steps 1-7a

---
+++ Cereal.java
@@ -0,0 +1,20 @@
+public class Cereal {
+    public static void main(String[] args) {
+        // Initialize variables
+        String name = "";
+        String type = "";
+        int nutrients = 0;
+        double weight = 0.0;
+        System.out.println(name.toString());
+        //Instance
+        public void Cereal(String name, String type, int nutrients, double weight) {
+            this.name = name;
+            this.type = type;
+            this.nutrients = nutrients;
+            this.weight = weight;
+        }
+
+       
+    }
+    
+}
